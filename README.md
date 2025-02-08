927af75768043bafdd15e75cf1427255340b85ce
rammfall <15bogdan17@gmail.com>
2025-02-08 16:49:22 +0200

    Complete

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # api-simple-notes
+
+## Clone repo to your machine
+## Open repo in terminal
+## Run ```npm i```
+## Run ```npm run start```
+## You can see message like this in terminal ```{"level":30,"time":1739025850880,"pid":62202,"hostname":"Bohdans-MacBook-Pro.local","msg":"Server listening at http://127.0.0.1:4400"}```
+## Documentation [here](http://localhost:4400/documentation) 
---
+++ index.js
@@ -0,0 +1,188 @@
+import fastify from 'fastify';
+import fastifyCors from '@fastify/cors';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
+import { Type } from '@sinclair/typebox';
+
+const server = fastify({
+  logger: true,
+}).withTypeProvider();
+
+server.register(fastifyCors);
+server.register(fastifySwagger);
+server.register(fastifySwaggerUi);
+
+/**
+ * @type {*[]}
+ */
+let notes = [
+  {
+    id: crypto.randomUUID(),
+    text: 'First note',
+  },
+];
+
+const getNotesDto = Type.Partial(
+  Type.Object({
+    query: Type.String({
+      minLength: 4,
+      maxLength: 100,
+    }),
+  }),
+  {
+    $id: 'GetNotesDTO',
+  },
+);
+
+const noteSchema = Type.Object(
+  {
+    id: Type.String(),
+    text: Type.String({
+      minLength: 6,
+      maxLength: 300,
+    }),
+  },
+  {
+    $id: 'NoteDTO',
+  },
+);
+
+const updatePayload = Type.Pick(noteSchema, ['text'], {
+  $id: 'CreateUpdatePayloadDTO',
+});
+
+const deletePayload = Type.Pick(noteSchema, ['id'], {
+  $id: 'IdPathPayloadDTO',
+});
+
+const noteList = Type.Array(Type.Ref(noteSchema), {
+  $id: 'NotesResponseListDTO',
+});
+
+server.addSchema(getNotesDto);
+server.addSchema(noteSchema);
+server.addSchema(noteList);
+server.addSchema(updatePayload);
+server.addSchema(deletePayload);
+
+server.register(
+  (instance, opts, done) => {
+    instance.get(
+      '/notes',
+      {
+        schema: {
+          tags: ['Notes'],
+          querystring: Type.Ref(getNotesDto),
+          response: {
+            200: Type.Ref(noteList),
+          },
+        },
+      },
+      (request, reply) => {
+        const { query } = request.query;
+
+        if (!query) {
+          return notes;
+        }
+
+        return notes.filter((note) =>
+          note.text.toLowerCase().includes(query.toLowerCase()),
+        );
+      },
+    );
+
+    instance.post(
+      '/notes',
+      {
+        schema: {
+          tags: ['Notes'],
+          body: Type.Ref(updatePayload),
+          response: {
+            200: Type.Ref(noteSchema),
+          },
+        },
+      },
+      (request, reply) => {
+        const { text } = request.body;
+        const note = {
+          id: crypto.randomUUID(),
+          text,
+        };
+        notes.push(note);
+
+        return note;
+      },
+    );
+
+    instance.patch(
+      '/users/:id',
+      {
+        schema: {
+          tags: ['Notes'],
+          params: Type.Ref(deletePayload),
+          body: Type.Ref(updatePayload),
+          response: {
+            200: Type.Ref(noteSchema),
+          },
+        },
+      },
+      (request, reply) => {
+        const { id } = request.params;
+        const { text } = request.body;
+
+        const note = notes.find((note) => note.id === id);
+
+        if (!note) {
+          return reply.status(404).send({
+            message: 'NotFound',
+          });
+        }
+
+        note.text = text;
+
+        return note;
+      },
+    );
+
+    instance.delete(
+      '/users/:id',
+      {
+        schema: {
+          tags: ['Notes'],
+          params: Type.Ref(deletePayload),
+          response: {
+            200: Type.Ref(noteSchema),
+          },
+        },
+      },
+      (request, reply) => {
+        const { id } = request.params;
+
+        const note = notes.find((note) => note.id === id);
+
+        if (!note) {
+          return reply.status(404).send({
+            message: 'NotFound',
+          });
+        }
+
+        notes = notes.filter((item) => item.id === id);
+
+        return note;
+      },
+    );
+
+    done();
+  },
+  {
+    prefix: '/api/v1',
+  },
+);
+
+server
+  .listen({
+    port: 4400,
+  })
+  .then(() => {
+    console.log('');
+  });
